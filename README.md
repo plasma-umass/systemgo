d5f0c77322063e302a9f966f84f34f0add41c248
Romans Volosatovs <b1101@riseup.net>
2016-05-24 00:56:23 +0200

    split unit package, added README, consistent parse

---
+++ README.md
@@ -0,0 +1,8 @@
+# Description
+And init system in Go, intended to run on [Browsix](https://github.com/plasma-umass/browsix) and other Unix-like OS(GNU/Linux incl.)
+# Features
+* Fast and concurrent
+* Handles dependencies well
+* [Systemd](https://github.com/Systemd/Systemd)-compatible
+
+Part of [GSoC project](https://summerofcode.withgoogle.com/projects/#6227933760847872)
--- unit/unit.go
+++ unit/unit.go
@@ -2,21 +2,11 @@ package unit
 
 import (
 	"errors"
-	"io"
 	"log"
-	"os"
 	"os/exec"
-	"reflect"
-	"strconv"
 	"strings"
 	"time"
-
-	"github.com/coreos/go-systemd/unit"
 )
-
-var supported = map[string]bool{
-	"simple": true,
-}
 
 var (
 	Units  map[string]*Unit
@@ -24,8 +14,6 @@ var (
 )
 
 var Errs = make(chan error, 1)
-
-//const MAXLENGTH = 2048
 
 // Struct representing the unit
 type Unit struct {
@@ -79,105 +67,6 @@ const (
 	Disabled
 	Enabled
 )
-
-// Looks for unit files in paths given and returns a map of units
-func ParseDir(paths ...string) (map[string]*Unit, error) {
-	units := map[string]*Unit{}
-	for _, path := range paths {
-		var err error
-		var file *os.File
-		var files []os.FileInfo
-
-		if file, err = os.Open(path); err != nil {
-			return nil, err
-		}
-		if files, err = file.Readdir(0); err != nil {
-			if err == io.EOF {
-				log.Println(path + " is empty")
-				err = nil
-				break
-			}
-			return nil, err
-		}
-
-		for _, f := range files {
-			var def *Definition
-
-			fpath := path + "/" + f.Name()
-
-			if f.IsDir() {
-				//return ParseDir(fpath)
-				//			return nil, errors.New("not implemented yet") //TODO
-				log.Println("recursive directory parsing not implemented yet")
-				continue
-			}
-			if file, err = os.Open(fpath); err != nil {
-				return nil, err
-			}
-			if def, err = ParseUnit(file); err != nil {
-				//return nil, errors.New("error parsing " + fpath + ": " + err.Error())
-				log.Println("error parsing " + fpath + ": " + err.Error())
-				continue
-			}
-
-			units[f.Name()] = &Unit{Definition: def /*, Status: Inactive, State: Static*/}
-		}
-	}
-	return units, nil
-}
-
-// Attempts to parse a specification of a unit
-func ParseUnit(specification io.Reader) (*Definition, error) {
-	var err error
-	var opts []*unit.UnitOption
-	definition := &Definition{}
-	def := reflect.ValueOf(definition).Elem()
-
-	if opts, err = unit.Deserialize(specification); err != nil {
-		return nil, err
-	}
-
-	for _, opt := range opts {
-		if v := def.FieldByName(opt.Section); v.IsValid() {
-			if v := v.FieldByName(opt.Name); v.IsValid() {
-				switch v.Kind() {
-				case reflect.SliceOf(reflect.TypeOf(reflect.String)).Kind():
-					values := strings.Split(opt.Value, " ")
-					v.Set(reflect.ValueOf(values))
-				case reflect.SliceOf(reflect.TypeOf(reflect.Int)).Kind():
-					values := strings.Split(opt.Value, " ")
-					ints := make([]int, len(values), len(values))
-					for i, val := range values {
-						if ints[i], err = strconv.Atoi(val); err != nil {
-							return nil, errors.New("Error parsing " + opt.Name + ": " + err.Error())
-						}
-					}
-					v.Set(reflect.ValueOf(ints))
-				default:
-					v.SetString(opt.Value)
-				}
-			} else {
-				return nil, errors.New("field " + opt.Name + " does not exist")
-			}
-		} else {
-			return nil, errors.New("section " + opt.Section + " does not exist")
-		}
-	}
-
-	if definition.Service.ExecStart == "" {
-		return nil, errors.New("'ExecStart' field not set")
-	}
-
-	if definition.Service.Type == "" {
-		definition.Service.Type = "simple"
-	}
-
-	if !supported[definition.Service.Type] {
-		return nil, errors.New("service type " + definition.Service.Type + " does not exist or is not supported yet")
-	}
-
-	return definition, nil
-}
 
 // Starts execution of the unit's specified command
 func (u *Unit) Start() (err error) {
@@ -265,8 +154,8 @@ func (u *Unit) Stop() (err error) {
 	}
 
 	err = u.Process.Kill()
-	//u.Loaded = false
 	delete(Loaded, u)
+
 	return
 }
 
@@ -286,7 +175,6 @@ func (u *Unit) Restart() (err error) {
 	u.Cmd = exec.Command(cmd[0], strings.Join(cmd[1:], " "))
 
 	err = u.Cmd.Start()
-	//u.Loaded = true
 	Loaded[u] = true
 	return
 }
