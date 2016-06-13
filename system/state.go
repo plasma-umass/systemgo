20f7a329fc33564a69cb5f36fcd66d84b34f2046
Romans Volosatovs <b1101@riseup.net>
2016-06-13 20:26:13 +0200

    state: fixed the stringer issue

--- system/state.go
+++ system/state.go
@@ -2,7 +2,7 @@ package system
 
 type State int
 
-// go:generate stringer -type=State  //TODO: find out why this breaks `go generate`
+//go:generate stringer -type=State state.go  //TODO: find out why this breaks `go generate`
 const (
 	Something State = iota // TODO: find all possible states
 	Degraded
